0364a2cf4065d2b8d21db7f5abe80e1a8dd86f70
Tofu Gang <tofugangsw@gmail.com>
2026-02-26 13:33:28 +0100

    create declarative tiles and puzzle box data so the puzzle can be created dynamically refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,37 @@
 import Tile from "./components/Tile.jsx";
+import { PuzzleBox, StartConfig } from "./lib/config.js";
 
 function App() {
     return (
-        <>
+        <div
+            className={`mx-auto mt-20 ${PuzzleBox.Colors.bg} ${PuzzleBox.Colors.border}`}
+            style={{
+                width: `${PuzzleBox.Dimensions.width + PuzzleBox.Dimensions.borderWidth * 2}px`,
+                height: `${PuzzleBox.Dimensions.height + PuzzleBox.Dimensions.borderWidth * 2}px`,
+                borderWidth: `${PuzzleBox.Dimensions.borderWidth}px` }}
+        >
+            {StartConfig.map((tile, index) => (
+                <Tile
+                    key={index}
+                    defaultPosition={tile.position}
+                    bounds={{
+                        left: 0,
+                        top: 0,
+                        right: PuzzleBox.Dimensions.width - tile.type.Dimensions.width,
+                        bottom: PuzzleBox.Dimensions.height - tile.type.Dimensions.height}}
+                    width={tile.type.Dimensions.width}
+                    height={tile.type.Dimensions.height}
+                    bgColor={tile.type.Colors.bg}
+                />
+            ))}
             <div
-                className="mx-auto mt-20 bg-amber-400 border-10 border-yellow-950"
-                style={{ width: "420px", height: "520px" }}
-            >
-                <Tile
-                    defaultPosition={{x: 0, y: 0}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-200-20 }}
-                    width={100}
-                    height={200}
-                    bgColor="bg-yellow-800"
-                />
-                <Tile
-                    defaultPosition={{x: 420-100-20, y: 0}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-200-20 }}
-                    width={100}
-                    height={200}
-                    bgColor="bg-yellow-800"
-                />
-                <Tile
-                    defaultPosition={{x: 0, y: 200}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-200-20 }}
-                    width={100}
-                    height={200}
-                    bgColor="bg-yellow-800"
-                />
-                <Tile
-                    defaultPosition={{x: 420-100-20, y: 200}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-200-20 }}
-                    width={100}
-                    height={200}
-                    bgColor="bg-yellow-800"
-                />
-                <Tile
-                    defaultPosition={{x: 100, y: 200}}
-                    bounds={{ left: 0, top: 0, right: 420-200-20, bottom: 520-100-20 }}
-                    width={200}
-                    height={100}
-                    bgColor="bg-yellow-800"
-                />
-                <Tile
-                    defaultPosition={{x: 100, y: 0}}
-                    bounds={{ left: 0, top: 0, right: 420-200-20, bottom: 520-200-20 }}
-                    width={200}
-                    height={200}
-                    bgColor="bg-black"
-                />
-                <Tile
-                    defaultPosition={{x: 0, y: 400}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-100-20 }}
-                    width={100}
-                    height={100}
-                    bgColor="bg-gray-500"
-                />
-                <Tile
-                    defaultPosition={{x: 420-100-20, y: 400}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-100-20 }}
-                    width={100}
-                    height={100}
-                    bgColor="bg-gray-500"
-                />
-                <Tile
-                    defaultPosition={{x: 100, y: 300}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-100-20 }}
-                    width={100}
-                    height={100}
-                    bgColor="bg-gray-500"
-                />
-                <Tile
-                    defaultPosition={{x: 200, y: 300}}
-                    bounds={{ left: 0, top: 0, right: 420-100-20, bottom: 520-100-20 }}
-                    width={100}
-                    height={100}
-                    bgColor="bg-gray-500"
-                />
-                <div className="relative bg-amber-400" style={{ width: "200px", height: "10px", left: "100px", top: "500px" }} />
-            </div>
-        </>
+                className={`relative ${PuzzleBox.Colors.bg}`}
+                style={{
+                    width: `${PuzzleBox.Dimensions.width / 2}px`,
+                    height: `${PuzzleBox.Dimensions.borderWidth}px`,
+                    left: `${PuzzleBox.Dimensions.width / 4}px`,
+                    top: `${PuzzleBox.Dimensions.height}px` }} />
+        </div>
     );
 }
 
